9956182773cb91e5f3194fb3899c6deab79f9379
Chris Coleman-Smith <gluon@jackdawjack.com>
2016-04-24 14:05:46 -0400

    removed some of the output pretty printing from the basic maze gen alg ;

--- src/gen_algs.cc
+++ src/gen_algs.cc
@@ -8,7 +8,7 @@
 // TODO(chris): figure out how to test this, does it even work?
 
 namespace {
-  std::default_random_engine rando; // stupid seed but that's ok for now
+  std::default_random_engine rando(1); // stupid seed but that's ok for now
 
   void bin_tree_helper(mazes::cell* cptr) {
     std::vector<mazes::cell*> neighbours;
@@ -18,18 +18,18 @@ namespace {
     if (cptr->get_east()) {
       neighbours.push_back(cptr->get_east());
     }
-    std::cout << cptr->to_s() << ":" << neighbours.size();
+    //std::cout << cptr->to_s() << ":" << neighbours.size();
 
     if (neighbours.size() > 0) {
       // pick one
       std::uniform_int_distribution<int> n_dist(0, neighbours.size()-1);
       int n_index = n_dist(rando);
-      std::cout << " idx: " << n_index;
+      //std::cout << " idx: " << n_index;
       mazes::cell* other = neighbours[n_index];
-      std::cout << " other: " << other->to_s();
+      //std::cout << " other: " << other->to_s();
       cptr->link(other, true);
     }
-    std::cout << std::endl;
+    //std::cout << std::endl;
   }
 } // end anonymous namespace
 
